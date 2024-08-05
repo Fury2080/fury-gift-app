317ed6c364425203b799faa5d6308c5f2a8a3d3f
Aryan Prince <aryanprincesanty@icloud.com>
2024-08-05 23:44:50 +0100

    🗃️ Create data bank of questions

---
+++ src/data/questions.ts
@@ -0,0 +1,27 @@
+export const questions = [
+  {
+    image: "/game1.jpg",
+    options: ["Resident Evil 7", "The Last of Us", "Silent Hill 2", "Days Gone"],
+    answer: "Days Gone",
+  },
+  {
+    image: "/game2.jpg",
+    options: ["Outlast", "Alien: Isolation", "Amnesia: The Dark Descent", "Resident Evil 2"],
+    answer: "Resident Evil 2",
+  },
+  {
+    image: "/game3.jpg",
+    options: ["Dead Space", "Until Dawn", "Evil Within", "Silent Hill 2"],
+    answer: "Silent Hill 2",
+  },
+  {
+    image: "/game4.jpg",
+    options: ["Resident Evil 7", "S.T.A.L.K.E.R.", "Metro Exodus", "Phasmophobia"],
+    answer: "Phasmophobia",
+  },
+  {
+    image: "/game5.jpg",
+    options: ["Alan Wake", "Fatal Frame", "Layers of Fear", "Cry of Fear"],
+    answer: "Cry of Fear",
+  },
+];
